780c7a5deb7b9c4aeb5b7dfa6376d7362d40786e
Juan Carlos Galvis Rodriguez <jcgalvis6@hotmail.com>
2021-01-20 00:12:29 -0500

    Add header component.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './header.scss'
+
+export const Header = () => {
+    return (
+      <header>
+          <h1>Movies</h1>
+      </header>  
+    );
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MoviesApp } from './MoviesApp';
 
+import './index.scss';
+
 const divRoot = document.querySelector("#root");
 
 ReactDOM.render(<MoviesApp />, divRoot);
