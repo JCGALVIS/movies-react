ae898c39c0cbe2a08d5a7f7b100783d9683cef98
Juan Carlos Galvis Rodriguez <jcgalvis6@hotmail.com>
2021-01-20 08:25:08 -0500

    Add search movies services.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,16 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./card.scss";
+import { urlImage } from "../../constants/constants";
 
-export const Card = ({ image, title, year }) => {
+export const Card = ({ image, title, date }) => {
+
   return (
     <div className="card">
       <div className="image">
-        <img src={image}></img>
+        <img src={`${urlImage}${image}`} alt={title}></img>
       </div>
       <div className='info'>
         <h3>{title}</h3>
-        <p>{year}</p>
+        <p>{date}</p>
       </div>
     </div>
   );
@@ -20,5 +22,5 @@ export const Card = ({ image, title, year }) => {
 Card.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  year: PropTypes.number.isRequired,
+  date: PropTypes.string.isRequired,
 };
---
+++ src/services/getListMovies.js
@@ -0,0 +1,18 @@
+import { apiKey, urlApi } from "../constants/constants"
+
+export const getListMovies = async() => {
+    const resource = 'search/movie';
+    const response = await fetch(`${urlApi}/${resource}?query=1&api_key=${apiKey}`);
+    const { results } =  await response.json();
+
+    const movies = results.map(movie => {
+        return {
+            id: movie.id,
+            image: movie.poster_path,
+            title: movie.original_title,
+            date: movie.release_date
+        }
+    });
+    
+    return movies;
+} 
